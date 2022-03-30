4b4d34987339a97e4bfa63594ecfbd843ace1ee0
jklaris <jakeklaristenfeld@yahoo.com>
2022-03-30 11:35:15 +0200

    add basic sign up

--- metabet/views/index.py
+++ metabet/views/index.py
@@ -5,7 +5,9 @@ URLs include:
 """
 import re
 import flask
+import flask_login
 import metabet
+from werkzeug.security import generate_password_hash, check_password_hash
 from metabet.model import get_db
 
 @metabet.app.route('/')
@@ -23,7 +25,6 @@ def show_login():
 def post_login():
     # make sure user can log in
 
-
     return flask.redirect(flask.url_for('show_vote'))
 
 @metabet.app.route('/poll', methods=['GET', 'POST'])
@@ -32,3 +33,33 @@ def show_vote():
     if flask.request.method == 'POST':
         context = {'post': 'POST REQUEST'}
     return flask.render_template('vote.html', **context)
+
+@metabet.app.route('/signup')
+def show_signup():
+    return flask.render_template('signup.html')
+
+@metabet.app.route('/signup', methods=['POST'])
+def signup():
+
+    user_id = flask.request.form.get('metamask')
+    password = flask.request.form.get('password')
+    password_2 = flask.request.form.get('password_copy')
+
+    if password_2 != password:
+        flask.flash('Passwords do not match. Try Again.')
+        return flask.redirect(flask.url_for('show_signup'))
+
+    # TODO: check user_id owns an nft 
+
+    add_owner(user_id, generate_password_hash(password, method='sha256'))
+
+    return flask.redirect(flask.url_for('show_login'))
+
+def add_owner(user, hashed_password, num_owns=1):
+    conn = get_db()
+    
+    query = 'INSERT INTO owners (`user_id`,`password`,`num_owns`) VALUES ({},{},{})'.format(sqlify_string(user),sqlify_string(hashed_password),num_owns)
+    conn.execute(query)
+
+def sqlify_string(word):
+    return '\'' + str(word) + '\''
